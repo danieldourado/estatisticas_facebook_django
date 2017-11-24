faaadac6edf2c224c3fa101db7fe4982c0d9ae32
Daniel Fernandes <daniel.dourado@gmail.com>
2017-11-24 22:24:53 +0000

    'created' is now shown in admin panel for PageInsights model

--- estatisticas_facebook/pages/admin.py
+++ estatisticas_facebook/pages/admin.py
@@ -4,6 +4,9 @@ from pages.models import Page, PageInsights
 class PageAdmin(admin.ModelAdmin):
     readonly_fields = ('created',)
 
+class PageInsightsAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
 # Register your models here.
 admin.site.register(Page,PageAdmin)
-admin.site.register(PageInsights)
+admin.site.register(PageInsights, PageInsightsAdmin)
