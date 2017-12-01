60af9c22b49eb99c189a4f05934010689e60d301
Daniel Fernandes <daniel.dourado@gmail.com>
2017-12-01 11:27:00 +0000

    Posts added to the django admin

--- estatisticas_facebook/posts/admin.py
+++ estatisticas_facebook/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from posts.models import *
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
 
 # Register your models here.
+admin.site.register(Post,PostAdmin)
