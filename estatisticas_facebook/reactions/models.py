f061e842bb9c61c669cbb77c26502008501876ec
Daniel Fernandes <daniel.dourado@gmail.com>
2017-12-17 22:07:19 +0000

    updated Reaction model

--- estatisticas_facebook/reactions/models.py
+++ estatisticas_facebook/reactions/models.py
@@ -1,12 +1,18 @@
 from django.core.urlresolvers import reverse
 from django.db import models
+from estatisticas_facebook.posts.models import Post
+from estatisticas_facebook.faceusers.models import FaceUsers
 
 
 class Reaction(models.Model):
-    name = models.CharField(max_length=255)
+    id                                      = models.CharField(primary_key = True, max_length = 45)
+    type                                    = models.CharField(default="", max_length=64)
+    user                                    = models.ForeignKey(FaceUsers, null=True)
+    post                                    = models.ForeignKey(Post, null=True)
+    name                                    = models.CharField(max_length = 512, default="")
 
     def __str__(self):
-        return self.name
+        return self.id
 
     def get_absolute_url(self):
         return reverse('reactions:detail', args=[str(self.id)])
