6b2f8b6f86d8798a8ef3af8c01da9df3b9697e92
Daniel Fernandes <daniel.dourado@gmail.com>
2017-12-17 22:17:42 +0000

    updated Paging model

--- estatisticas_facebook/pagings/models.py
+++ estatisticas_facebook/pagings/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 
 class Paging(models.Model):
-    name = models.CharField(max_length=255)
+    model_name  = models.CharField(default="", max_length=64)
+    url         = models.CharField(default="", max_length=512)
+    name        = models.CharField(max_length = 512, default="")
 
     def __str__(self):
-        return self.name
+        return self.id
 
     def get_absolute_url(self):
         return reverse('pagings:detail', args=[str(self.id)])
