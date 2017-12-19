87603f6736bac5830dd4fa0afe850bdbe742ee35
Daniel Fernandes <daniel.dourado@gmail.com>
2017-12-19 18:44:06 +0000

    Post.message can be null

--- estatisticas_facebook/posts/models.py
+++ estatisticas_facebook/posts/models.py
@@ -5,15 +5,8 @@ from django.core.urlresolvers import reverse
 from util.graph import *
 from pages.models import *
 
-def getPostInfo(page_model, since):
-    postsQuery = '/posts?fields=id,name,created_time,story,message,permalink_url,shares.summary(count).as(shares),comments.limit(1),reactions.limit(1),comments.limit(0).summary(total_count).as(total_comments),insights.metric(post_reactions_by_type_total)&pretty=false&since='+str(since)
-    raw_json = getNewGraphApi(page_model.id).get_object(page_model.id+postsQuery)
-    
-    data = raw_json['data']
-    paging = raw_json['paging']
-    
-    Post.objects.filter(page = page_model).delete()    
-    
+
+def savePostData(page_model, data):
     for post in data:
         insights_values = post['insights']['data'][0]['values'][0]['value']
        
@@ -23,18 +16,18 @@ def getPostInfo(page_model, since):
             
         temp_post = Post(
             page = page_model,
-            id = post['id'],
-            created_time = post['created_time'],
-            message = post['message'],
-            permalink_url = post['permalink_url'],
+            id = post.get('id'),
+            created_time = post.get('created_time'),
+            message = post.get('message'),
+            permalink_url = post.get('permalink_url'),
             shares = shares,
             total_comments = post['total_comments']['summary']['total_count'],
-            post_reactions_like_total = insights_values['like'],
-            post_reactions_love_total = insights_values['love'],
-            post_reactions_wow_total = insights_values['wow'],
-            post_reactions_haha_total = insights_values['haha'],
-            post_reactions_sorry_total = insights_values['sorry'],
-            post_reactions_anger_total = insights_values['anger'],
+            post_reactions_like_total = insights_values.get('like'),
+            post_reactions_love_total = insights_values.get('love'),
+            post_reactions_wow_total = insights_values.get('wow'),
+            post_reactions_haha_total = insights_values.get('haha'),
+            post_reactions_sorry_total = insights_values.get('sorry'),
+            post_reactions_anger_total = insights_values.get('anger'),
             reactions   =insights_values['like']
                         +insights_values['love']
                         +insights_values['wow']
@@ -44,6 +37,17 @@ def getPostInfo(page_model, since):
             )
         temp_post.save()
         print('new post saved: '+temp_post.id)
+        
+def getPostInfo(page_model, since):
+    postsQuery = '/posts?fields=id,name,created_time,story,message,permalink_url,shares.summary(count).as(shares),comments.limit(1),reactions.limit(1),comments.limit(0).summary(total_count).as(total_comments),insights.metric(post_reactions_by_type_total)&pretty=false&limit=50&since='+str(since)
+    raw_json = getNewGraphApi(page_model.id).get_object(page_model.id+postsQuery)
+    
+    data = raw_json['data']
+    paging = raw_json['paging']
+    
+    Post.objects.filter(page = page_model).delete()    
+    
+    savePostData(page_model, data)
         
     
     
@@ -63,8 +67,8 @@ class Post(models.Model):
     post_reactions_negativo_total           = models.IntegerField(default=0)
     post_reactions_positivo_porcentagem     = models.IntegerField(default=0)
     post_reactions_negativo_porcentagem     = models.IntegerField(default=0)
-    created_time                            = models.CharField(max_length = 45)
-    message                                 = models.CharField(max_length = 4500)
+    created_time                            = models.CharField(max_length = 45, null=True)
+    message                                 = models.CharField(max_length = 4500, null=True)
     permalink_url                           = models.CharField(max_length = 450, default="")
     created                                 = models.DateTimeField(auto_now_add=True)
     name                                    = models.CharField(max_length = 450, default="")
