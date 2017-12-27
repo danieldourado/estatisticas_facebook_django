5c91e158c0eb30571691e8f819c1568a7f53cfe7
Daniel Fernandes <daniel.dourado@gmail.com>
2017-12-27 21:42:44 +0000

    replaced home with pages:list

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from estatisticas_facebook.pages.views import PageListView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    
+    url(r'^$', PageListView.as_view(), name='home'),
+#    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
