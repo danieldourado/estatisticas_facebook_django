67274e647cf9e2261f1a7810cd9862a4040dfc06
Daniel Fernandes <daniel.dourado@gmail.com>
2018-07-04 13:42:22 +0000

    adicionou comments toplevel e comments stream, retirou todos os cálculos

--- estatisticas_facebook/posts/models.py
+++ estatisticas_facebook/posts/models.py
@@ -9,7 +9,8 @@ from estatisticas_facebook.faceusers.models import *
 import dateutil.parser
 
 
-QUERY = '/posts?fields=id,name,created_time,story,message,permalink_url,shares.summary(count).as(shares),comments.limit(1),comments.limit(0).summary(total_count).as(total_comments),insights.metric(post_reactions_by_type_total,post_impressions,post_impressions_unique)&pretty=false&limit=100&since='
+#QUERY = '/posts?fields=id,name,created_time,story,message,permalink_url,shares.summary(count).as(shares),comments.limit(1),comments.limit(0).summary(total_count).as(total_comments),insights.metric(post_reactions_by_type_total,post_impressions,post_impressions_unique)&pretty=false&limit=100&since='
+QUERY = '/posts?fields=id,name,created_time,story,message,permalink_url,shares.summary(count).as(shares),comments.limit(1),comments.limit(0).summary(total_count).filter(toplevel).as(total_comments_toplevel),comments.limit(0).summary(total_count).filter(stream).as(total_comments_stream),insights.metric(post_reactions_by_type_total,post_impressions,post_impressions_unique)&pretty=false&limit=100&since='
 
 def save_post_data(page_model, data):
     for post in data:
@@ -36,19 +37,21 @@ def save_post_data(page_model, data):
     
         total_reactions=post_reactions_like_total+post_reactions_love_total+post_reactions_wow_total+post_reactions_haha_total+post_reactions_sorry_total+post_reactions_anger_total
             
-        total_comments = post.get('total_comments').get('summary').get('total_count')
-        engajamento = total_reactions+shares+total_comments
-        taxa_de_engajamento=0
-        if post_impressions != 0:
-            taxa_de_engajamento = (engajamento/post_impressions)*100
-        post_reactions_positivo_total = post_reactions_like_total+post_reactions_love_total+post_reactions_wow_total+shares
-        post_reactions_negativo_total = post_reactions_anger_total+post_reactions_haha_total+post_reactions_sorry_total+total_comments
-        positivo_mais_negativo = post_reactions_positivo_total+post_reactions_negativo_total
-        post_reactions_positivo_porcentagem=0
-        post_reactions_negativo_porcentagem=0
-        if positivo_mais_negativo != 0:
-        	post_reactions_positivo_porcentagem = post_reactions_positivo_total/positivo_mais_negativo*100
-        	post_reactions_negativo_porcentagem = post_reactions_negativo_total/positivo_mais_negativo*100
+        total_comments_toplevel = post.get('total_comments_toplevel').get('summary').get('total_count')
+        total_comments_stream = post.get('total_comments_stream').get('summary').get('total_count')
+        #engajamento_toplevel = total_reactions+shares+total_comments_toplevel
+        #engajamento_stream = total_reactions+shares+total_comments_stream
+        #taxa_de_engajamento=0
+        #if post_impressions != 0:
+        #    taxa_de_engajamento = (engajamento/post_impressions)*100
+        #post_reactions_positivo_total = post_reactions_like_total+post_reactions_love_total+post_reactions_wow_total+shares
+        #post_reactions_negativo_total = post_reactions_anger_total+post_reactions_haha_total+post_reactions_sorry_total+total_comments
+        #positivo_mais_negativo = post_reactions_positivo_total+post_reactions_negativo_total
+        #post_reactions_positivo_porcentagem=0
+        #post_reactions_negativo_porcentagem=0
+        #if positivo_mais_negativo != 0:
+        #  	post_reactions_positivo_porcentagem = post_reactions_positivo_total/positivo_mais_negativo*100
+        #	post_reactions_negativo_porcentagem = post_reactions_negativo_total/positivo_mais_negativo*100
         
         temp_post = Post(
             page = page_model,
@@ -57,22 +60,23 @@ def save_post_data(page_model, data):
             message = post.get('message'),
             permalink_url = post.get('permalink_url'),
             shares = shares,
-            total_comments = total_comments,
+            total_comments_toplevel = total_comments_toplevel,
+            total_comments_stream = total_comments_stream,
             post_reactions_like_total = post_reactions_like_total,
             post_reactions_love_total = post_reactions_love_total,
             post_reactions_wow_total = post_reactions_wow_total,
             post_reactions_haha_total = post_reactions_haha_total,
             post_reactions_sorry_total = post_reactions_sorry_total,
             post_reactions_anger_total = post_reactions_anger_total,
-            post_reactions_positivo_total=post_reactions_positivo_total,
-            post_reactions_negativo_total=post_reactions_negativo_total,
-            post_reactions_positivo_porcentagem=post_reactions_positivo_porcentagem,
-            post_reactions_negativo_porcentagem=post_reactions_negativo_porcentagem,
+        #    post_reactions_positivo_total=post_reactions_positivo_total,
+        #    post_reactions_negativo_total=post_reactions_negativo_total,
+        #    post_reactions_positivo_porcentagem=post_reactions_positivo_porcentagem,
+        #    post_reactions_negativo_porcentagem=post_reactions_negativo_porcentagem,
             reactions = total_reactions,
             post_impressions=post_impressions,
             post_impressions_unique=post_impressions_unique,
-            engajamento=engajamento,
-            taxa_de_engajamento=taxa_de_engajamento,
+        #    engajamento=engajamento,
+        #    taxa_de_engajamento=taxa_de_engajamento,
             )
         temp_post.save()
         
@@ -111,7 +115,8 @@ def getPostInfo(page_model, since):
 class Post(models.Model):
     id                                      = models.CharField(primary_key = True, max_length = 45)
     page                                    = models.ForeignKey('pages.Page', on_delete=models.CASCADE)
-    total_comments                          = models.IntegerField(default=0)
+    total_comments_stream                   = models.IntegerField(default=0)
+    total_comments_toplevel                 = models.IntegerField(default=0)
     shares                                  = models.IntegerField(default=0)
     reactions                               = models.IntegerField(default=0)
     post_reactions_like_total               = models.IntegerField(default=0)
@@ -120,14 +125,15 @@ class Post(models.Model):
     post_reactions_haha_total               = models.IntegerField(default=0)
     post_reactions_sorry_total              = models.IntegerField(default=0)
     post_reactions_anger_total              = models.IntegerField(default=0)
-    post_reactions_positivo_total           = models.IntegerField(default=0)
-    post_reactions_negativo_total           = models.IntegerField(default=0)
-    post_reactions_positivo_porcentagem     = models.FloatField(default=0)
-    post_reactions_negativo_porcentagem     = models.FloatField(default=0)
+    #post_reactions_positivo_total           = models.IntegerField(default=0)
+    #post_reactions_negativo_total           = models.IntegerField(default=0)
+    #post_reactions_positivo_porcentagem     = models.FloatField(default=0)
+    #post_reactions_negativo_porcentagem     = models.FloatField(default=0)
     post_impressions                        = models.IntegerField(default=0)
     post_impressions_unique                 = models.IntegerField(default=0)
-    engajamento                             = models.IntegerField(default=0)
-    taxa_de_engajamento                     = models.FloatField(default=0)
+    #engajamento_toplevel                    = models.IntegerField(default=0)
+    #engajamento_stream                      = models.IntegerField(default=0)
+    #taxa_de_engajamento                     = models.FloatField(default=0)
     created_time                            = models.CharField(max_length = 45, null=True)
     message                                 = models.CharField(max_length = 18000, null=True)
     permalink_url                           = models.CharField(max_length = 450, null=True)
